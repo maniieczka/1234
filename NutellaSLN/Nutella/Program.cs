1d239feb158a8684975e83e39a46e2df20fb3bb4
maniieczka <maniieczka@protonmail.com>
2023-08-18 13:59:15 +0200

    Update Program.cs

--- NutellaSLN/Nutella/Program.cs
+++ NutellaSLN/Nutella/Program.cs
@@ -4,10 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            string mama;
-            mama=Console.ReadLine();
-            Console.WriteLine("to co wpisałaś to: "+mama);
-        }
+            Console.WriteLine("Siema,celem aplikacji jest to zebys podał dwie liczby całkowite i otrzymał wynik dodawania lub odejmowania");
+            Console.WriteLine("Jeśli chcesz otrzymać wynik dodawania wpisz +, jeśli chcesz otrzymać wynik odejmowania wpisz -");
+            string działanie;
+            działanie = Console.ReadLine();
+            if (działanie == "+")
+            {
+                Console.WriteLine("Otrzymasz wynik dodawania, podaj pierwszą liczbę całkowitą");
+                string pierwszaLiczbaCałkowita = Console.ReadLine();
+                int Pierwsza = int.Parse(pierwszaLiczbaCałkowita);
+                Console.WriteLine("Otrzymasz wynik dodawania, podaj drugą liczbę całkowitą");
+                string drugaLiczbaCałkowita = Console.ReadLine();
+                int Druga = int.Parse(drugaLiczbaCałkowita);
+                int Wynik = Pierwsza + Druga;
+                Console.WriteLine("Wynik to:" + Wynik);
+            }
+            if (działanie == "-")
+            {
+                Console.WriteLine("Otrzymasz wynik odejmowania, podaj pierwszą liczbę całkowitą");
+                string pierwszaLiczbaCałkowita = Console.ReadLine();
+                int Pierwsza = int.Parse(pierwszaLiczbaCałkowita);
+                Console.WriteLine("Otrzymasz wynik odejmowania, podaj drugą liczbę całkowitą");
+                string drugaLiczbaCałkowita = Console.ReadLine();
+                int Druga = int.Parse(drugaLiczbaCałkowita);
+                int Wynik = Pierwsza - Druga;
+                Console.WriteLine("Wynik to:" + Wynik);
+            }
+        } 
     }
 }
