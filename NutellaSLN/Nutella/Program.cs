e22a1551cf330e179c076f373300d2b8e75416b6
maniieczka <maniieczka@protonmail.com>
2023-08-18 12:55:37 +0200

    nowe

---
+++ NutellaSLN/Nutella/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Nutella
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string mama;
+            mama=Console.ReadLine();
+            Console.WriteLine("to co wpsiałaś to: "+mama);
+        }
+    }
+}
