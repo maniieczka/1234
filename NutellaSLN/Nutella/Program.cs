35381603155cfb639a08d84d062ed31d9b362d5f
maniieczka <maniieczka@protonmail.com>
2023-08-19 13:50:24 +0200

    Update Program.cs

--- NutellaSLN/Nutella/Program.cs
+++ NutellaSLN/Nutella/Program.cs
@@ -4,8 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Siema,celem aplikacji jest to zebys podał dwie liczby całkowite i otrzymał wynik dodawania lub odejmowania");
-            Console.WriteLine("Jeśli chcesz otrzymać wynik dodawania wpisz +, jeśli chcesz otrzymać wynik odejmowania wpisz -");
+            Console.WriteLine("Siema,celem aplikacji jest to zebys podał dwie liczby całkowite i otrzymał wynik dodawania, odejmowania, mnożenia, dzielenia.");
+            Console.WriteLine("Jeśli chcesz otrzymać wynik dodawania wpisz +, jeśli chcesz otrzymać wynik odejmowania wpisz -, mnożenie wpisz *, dzielenie wpisz /");
             string działanie;
             działanie = Console.ReadLine();
             if (działanie == "+")
@@ -30,6 +30,28 @@
                 int Wynik = Pierwsza - Druga;
                 Console.WriteLine("Wynik to:" + Wynik);
             }
-        } 
+            if (działanie == "*")
+            {
+                Console.WriteLine("Otrzymasz wynik mnożenia, podaj pierwszą liczbę całkowitą");
+                string pierwszaLiczbaCałkowita = Console.ReadLine();
+                int Pierwsza = int.Parse(pierwszaLiczbaCałkowita);
+                Console.WriteLine("Otrzymasz wynik mnożenia, podaj drugą liczbę całkowitą");
+                string drugaLiczbaCałkowita = Console.ReadLine();
+                int Druga = int.Parse(drugaLiczbaCałkowita);
+                int Wynik = Pierwsza * Druga;
+                Console.WriteLine("Wynik to:" + Wynik);
+            }
+            if (działanie == "/")
+            {
+                Console.WriteLine("Otrzymasz wynik dzielenia, podaj pierwszą liczbę całkowitą");
+                string pierwszaLiczbaCałkowita = Console.ReadLine();
+                int Pierwsza = int.Parse(pierwszaLiczbaCałkowita);
+                Console.WriteLine("Otrzymasz wynik dzielenia, podaj drugą liczbę całkowitą");
+                string drugaLiczbaCałkowita = Console.ReadLine();
+                int Druga = int.Parse(drugaLiczbaCałkowita);
+                int Wynik = Pierwsza / Druga;
+                Console.WriteLine("Wynik to:" + Wynik);
+            }
+        }
     }
 }
